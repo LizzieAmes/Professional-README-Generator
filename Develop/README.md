4b6aa64c824ab0cc441cc2e9babcd78d60f09d7e
Lizzie <lizziejacoboo@gmail.com>
2024-01-06 14:41:47 -0700

    finished generateMarkdown

---
+++ Develop/README.md
@@ -0,0 +1,32 @@
+# Professional README Generator
+
+  ![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)
+  ## Description
+  This project creates a professional README file.
+  
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  ## Installation
+  Install Node.js.
+  
+  ## Usage
+  Use to create README.
+  
+  ## Contributing
+  Contributions are welcome.
+  
+  ## Tests
+  Test by running node index.
+  
+  ## License
+  This Project is licensed under MIT License - see the [LICENSE](https://opensource.org/licenses/MIT) file for details
+  
+  ## Questions
+  For any questions, please contact me at [lizziejacoboo@gmail.com](mailto:lizziejacoboo@gmail.com).
+  GitHub Profile: [lizzieames](https://github.com/lizzieames)
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -14,16 +14,58 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (!license || license === 'Other') return '';
+  
+  const licenseUrls = {
+    'MIT': 'https://opensource.org/licenses/MIT',
+    'Apache': 'https://opensource.org/licenses/Apache-2.0',
+    'GPL': 'https://www.gnu.org/licenses/gpl-3.0'
+  };
+  return licenseUrls[license] || '';
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (!license || license === 'Other') return '';
+  return `## License
+  This Project is licensed under ${license} License - see the [LICENSE](${renderLicenseLink(license)}) file for details`;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ${renderLicenseBadge(data.license)}
+  ## Description
+  ${data.description}
+  
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  ## Installation
+  ${data.installation}
+  
+  ## Usage
+  ${data.usage}
+  
+  ## Contributing
+  ${data.contributing}
+  
+  ## Tests
+  ${data.test}
+  
+  ${renderLicenseSection(data.license)}
+  
+  ## Questions
+  For any questions, please contact me at [${data.email}](mailto:${data.email}).
+  GitHub Profile: [${data.github}](https://github.com/${data.github})
 `;
 }
 
